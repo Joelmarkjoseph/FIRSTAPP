dbb5cc894e1fa5baf57c0e1058c752d88fd083ab
Joel Mark Joseph S <joelmarkjoseph2004@gmail.com>
2024-08-26 02:34:20 +0530

    Newwww

--- backend/app.py
+++ backend/app.py
@@ -58,8 +58,6 @@ def delete_contact(user_id):
 
     return jsonify({"message":"User Deleted"}), 200
 
-# if __name__ == "__main__":
-#     with app.app_context():
-#         db.create_all()
-
-#     app.run(debug=False, host='0.0.0.0') 
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
