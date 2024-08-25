8fe16433886733a99c3924cc39b0b8e8c0242407
Joel Mark Joseph S <joelmarkjoseph2004@gmail.com>
2024-08-26 03:25:23 +0530

    nwerrr

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   const [currentContact, setCurrentContact] = useState({});
 
   useEffect(() => {
-    // fetchContacts();
+    fetchContacts();
   }, []);
   const fetchContacts = async () => {
     const response = await fetch(
@@ -41,6 +41,7 @@ function App() {
 
   return (
     <>
+      <h1>Hello</h1>
       <ContactsList
         contacts={contacts}
         updateContact={openEditModal}
