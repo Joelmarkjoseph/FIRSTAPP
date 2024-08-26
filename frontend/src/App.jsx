27216bf0a6483ee9c80d5fc5156df47e152c3b10
Joel Mark Joseph S <joelmarkjoseph2004@gmail.com>
2024-08-26 12:02:06 +0530

    New Commits

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,59 +4,64 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [contacts, setContacts] = useState([]);
-  const [isModalOpen, setModalOpen] = useState(false);
-  const [currentContact, setCurrentContact] = useState({});
+  // const [contacts, setContacts] = useState([]);
+  // const [isModalOpen, setModalOpen] = useState(false);
+  // const [currentContact, setCurrentContact] = useState({});
 
-  useEffect(() => {
-    fetchContacts();
-  }, []);
-  const fetchContacts = async () => {
-    const response = await fetch(
-      "https://firstapp-1-wbeh.onrender.com/contacts"
-    );
-    const data = await response.json();
-    setContacts(data.contacts);
-    console.log(data.contacts);
-  };
+  // useEffect(() => {
+  //   fetchContacts();
+  // }, []);
+  // const fetchContacts = async () => {
+  //   const response = await fetch(
+  //     "https://firstapp-1-wbeh.onrender.com/contacts"
+  //   );
+  //   const data = await response.json();
+  //   setContacts(data.contacts);
+  //   console.log(data.contacts);
+  // };
 
-  const closeModal = () => {
-    setModalOpen(false);
-    setCurrentContact({});
-  };
-  const openCreateModal = () => {
-    if (!isModalOpen) setModalOpen(true);
-  };
+  // const closeModal = () => {
+  //   setModalOpen(false);
+  //   setCurrentContact({});
+  // };
+  // const openCreateModal = () => {
+  //   if (!isModalOpen) setModalOpen(true);
+  // };
 
-  const openEditModal = (contact) => {
-    if (isModalOpen) return;
-    setCurrentContact(contact);
-    setModalOpen(true);
-  };
+  // const openEditModal = (contact) => {
+  //   if (isModalOpen) return;
+  //   setCurrentContact(contact);
+  //   setModalOpen(true);
+  // };
 
-  const onUpdate = () => {
-    closeModal();
-    fetchContacts();
-  };
+  // const onUpdate = () => {
+  //   closeModal();
+  //   fetchContacts();
+  // };
 
+  // return (
+  //   <>
+  //     <ContactsList
+  //       contacts={contacts}
+  //       updateContact={openEditModal}
+  //       updateCallback={onUpdate}
+  //     />
+  //     <button onClick={openCreateModal}>Create New Contact</button>
+  //     {isModalOpen && (
+  //       <div className="modal">
+  //         <div className="modal-content">
+  //           <span className="close" onClick={closeModal}>
+  //             &times;
+  //           </span>
+  //           <ContactForm existingContact={currentContact} />
+  //         </div>
+  //       </div>
+  //     )}
+  //   </>
+  // );
   return (
     <>
-      <ContactsList
-        contacts={contacts}
-        updateContact={openEditModal}
-        updateCallback={onUpdate}
-      />
-      <button onClick={openCreateModal}>Create New Contact</button>
-      {isModalOpen && (
-        <div className="modal">
-          <div className="modal-content">
-            <span className="close" onClick={closeModal}>
-              &times;
-            </span>
-            <ContactForm existingContact={currentContact} />
-          </div>
-        </div>
-      )}
+      <h1>Hellooooooo</h1>
     </>
   );
 }
